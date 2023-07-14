3baada0bb8ec2fd5b7fa1ad1f60608a6cca0c341
chaseseidel <chaseseidel16@gmail.com>
2023-07-14 13:44:05 -0700

    Add button for contact page

--- src/pages/buttons.js
+++ src/pages/buttons.js
@@ -1,12 +1,14 @@
 import createHomePage from "./home";
 import createMenuPage from "./menu";
 import createAboutPage from "./about";
+import createContactPage from "./contact";
 import resetPage from "./reset";
 
 export default function createButtonListeners(element) {
     const homeButton = document.getElementById('home');
     const menuButton = document.getElementById('menu');
     const aboutButton = document.getElementById('about');
+    const contactButton = document.getElementById('contact');
 
     homeButton.addEventListener('click', () => {
         resetPage(element);
@@ -22,4 +24,9 @@ export default function createButtonListeners(element) {
         resetPage(element);
         createAboutPage(element);
     });
+
+    contactButton.addEventListener('click', () => {
+        resetPage(element);
+        createContactPage(element);
+    })
 }
